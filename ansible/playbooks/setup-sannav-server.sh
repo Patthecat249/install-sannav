f1ae7b2302a20a932415cc8cfdde960fa1e1fc60
Patrick Jahn <privat@patrickjahn.de>
2025-03-10 10:57:22 +0100

    Add some Text

--- ansible/playbooks/setup-sannav-server.sh
+++ ansible/playbooks/setup-sannav-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create yum-repos
-
+echo "Create appstream.repo"
 cat << EOF > /etc/yum.repos.d/appstream.repo
 [AppStream]
 name=Red Hat Enterprise Linux 9.5.0 - AppStream
@@ -9,6 +9,7 @@ enabled=1
 gpgcheck=0
 EOF
 
+echo "Create baseos.repo"
 cat << EOF > /etc/yum.repos.d/baseos.repo
 [BaseOS]
 name=Red Hat Enterprise Linux 9.5.0 - BaseOS
@@ -18,22 +19,30 @@ gpgcheck=0
 EOF
 
 # Ordner anlegen
+echo "Create folders"
 mkdir -p /opt/sva/downloads /opt/sva/git /mnt/cdrom
 
 # CDROM mounten und Subscription-Manager deaktivieren
+echo "Disable Subscription Manager"
 subscription-manager config --rhsm.manage_repos=0
+
+echo "Mount CDROM"
 mount -o loop /dev/sr0 /mnt/cdrom/
 
 # Install ansible and git
+echo "Install ansible and git"
 dnf install -y ansible-core.x86_64 git
 
 # Go to folder
+echo "Go to git /opt/sva/git"
 cd /opt/sva/git
 
 # Clone repository
+echo "Clone Repository"
 git clone https://github.com/Patthecat249/install-sannav.git
 
 # SSH-Key anlegen
+echo "Create ssh-key"
 ssh-keygen -t rsa -N ''
 
-# ssh-copy-id 10.0.249.192
+# ssh-copy-id 10.0.249.42
